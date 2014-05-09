0eabfa828d4c4c0db55b584825e96d862d81824f
Sneha Ashtikar <snehanashtikar@gmail.com>
2014-05-09 00:35:39 -0700

    Sneha: Added resizable panels

--- cqb.js
+++ cqb.js
@@ -2,7 +2,40 @@ jsPlumb.ready(function() {
 	var cqb = new CQB({
 		elementId: "cqb_container"
 	});
-	cqb.init()
+	cqb.init();
+	$("#right_panel").outerWidth($("#cqb_container").innerWidth()- $("#table_panel").outerWidth(true)-1);
+	$("#right_panel").outerHeight($("#cqb_container").innerHeight());
+	$("#output_panel").outerHeight($("#right_panel").innerHeight()/3);
+	$("#query_panel").outerHeight($("#right_panel").innerHeight()/3);
+	$("#er_panel").outerHeight($("#right_panel").innerHeight()/3);
+	$("#table_panel").resizable({
+		 handles: 'e',
+        minWidth: '100',
+        maxWidth: '350',
+        resize: function() {
+            var remainingSpace = $(this).parent().width() - $(this).outerWidth(),
+                divTwo = $(this).next(),
+                divTwoWidth = remainingSpace - (divTwo.outerWidth() - divTwo.width());
+            divTwo.width(divTwoWidth);
+        }
+
+		});
+
+	//$("#er_panel").height($("#right_panel").parent().innerHeight()-$("#output_panel").outerHeight()-$("#query_panel").outerHeight()-1);
+	$("#er_panel").resizable({
+		 handles: 's',
+        minHeight: ($("#right_panel").innerHeight()/10),
+        maxHeight: ($("#right_panel").innerHeight()/2),
+        resize: function() {
+            var remainingSpace = $(this).parent().height() - $(this).outerHeight()- $("#query_panel").outerHeight(),
+                divTwo = $(this).next(),
+                divTwoHeight = remainingSpace - (divTwo.outerHeight() - divTwo.height());
+            divTwo.height(divTwoHeight);
+        }
+
+		});
+
+	//$("#right_panel").resizable();
 });
 
 function CQB(opts) {
@@ -21,13 +54,17 @@ function CQB(opts) {
 	this.tableList = [];
 
 	this.init = function() {
-		this.element.html("<div id='table_list'></div><div id='er-diagram' class='er-diagram'><h1>Drag Tables Here</div>");		
+		//this.element.html("<div id='table_list' ></div><div id='er-diagram' class='er-diagram'><h1>Drag Tables Here</h1></div>");
+		this.element.html("<div id='table_panel' class='panel-default'><div class='panel-heading'>Table List</div><div id='table_list' class='panel-body '></div></div><div id='right_panel'class='panel-default'><div id = 'er_panel' class='innerPanel panel-default'><div class='panel-heading'>Query Builder</div><div id='er-diagram' class='er-diagram panel-body'><h1>Drag Tables Here</h1></div></div><div id = 'output_panel'class='innerPanel panel-default'><div class='panel-heading'>Query</div></div><div id = 'query_panel'class='innerPanel panel-default'><div class='panel-heading'>Query</div></div></div>");
+		
 		this.tableList = new TableList({
 			element: "table_list",
-			tables: this.tables_list
+			tables: this.tables_list,
+			container_class: "table"
 		});
 		this.erDiagram = new ERDiagram();		
 		this.tableList.draw();
+		
 		this.erDiagram.draw();
 		this.attachEvents();
 
@@ -71,16 +108,19 @@ function CQB(opts) {
 function TableList(opts) { 
 	this.elementId = opts.element;
 	this.element = $("#" + this.elementId);
+	//this.element.resizable();
+	
 	this.tables = opts.tables;
 	this.container_class = opts.container_class || "table-container";
 	this.cell_class = opts.cell_class || "table-cell";
+	//this.element.addClass("ui-widget-content");
 	this.draw = function() {
 		var html = "";
 		for(var i = 0; i < this.tables.length; i++) {
 			html += "<div class='" + this.cell_class + "' id='" + this.tables[i].name + "'>" + this.tables[i].name + " </div>";
 		}
 
-
+		$(this.cell_class).removeClass("table-container");
 		if(!this.element.hasClass(this.container_class)) {
 			this.element.addClass(this.container_class);
 		}
