4e2b434f9924c5ce6ddc9eb9ecab2f53bba4db1c
Sneha Ashtikar <snehanashtikar@gmail.com>
2014-06-03 16:29:02 -0700

    Sneha: adding relations 2

--- cqb.js
+++ cqb.js
@@ -41,21 +41,21 @@ jsPlumb.ready(function() {
 			active:false
 		});
 
-	 instance = window.instance =jsPlumb.getInstance({
-			// default drag options
-			DragOptions : { cursor: 'pointer', zIndex:2000 },
-			// the overlays to decorate each connection with.  note that the label overlay uses a function to generate the label text; in this
-			// case it returns the 'labelText' member that we set on each connection in the 'init' method below.
-			ConnectionOverlays : [
-				[ "Arrow", { location:1 } ],
-				[ "Label", { 
-					location:0.1,
-					id:"label",
-					cssClass:"aLabel"
-				}]
-			],
-			Container:"er-diagram"
-		});
+	 // instance = window.instance =jsPlumb.getInstance({
+		// 	// default drag options
+		// 	DragOptions : { cursor: 'pointer', zIndex:2000 },
+		// 	// the overlays to decorate each connection with.  note that the label overlay uses a function to generate the label text; in this
+		// 	// case it returns the 'labelText' member that we set on each connection in the 'init' method below.
+		// 	ConnectionOverlays : [
+		// 		[ "Arrow", { location:1 } ],
+		// 		[ "Label", { 
+		// 			location:0.1,
+		// 			id:"label",
+		// 			cssClass:"aLabel"
+		// 		}]
+		// 	],
+		// 	Container:"er-diagram"
+		// });
 	
 });
 
@@ -218,7 +218,8 @@ function ERDiagram(opts) {
 				html += table.fields[j] ;
 				html += "</div>";
 
-				flowchart._addEndpoints(table.name+"_"+table.fields[j],["LeftMiddle"], ["RightMiddle"]);
+				jsPlumb.addEndpoint(table.name+"_"+table.fields[j], flowchart.sourceEndpoint, { anchor:"LeftMiddle", uuid:table.name+"_"+table.fields[j]+"_LeftMiddle"});
+				//flowchart._addEndpoints(table.name+"_"+table.fields[j],["LeftMiddle"], ["RightMiddle"]);
 
 			}
 			
@@ -278,20 +279,20 @@ function Flowchart(){
 			joinstyle:"round",
 			outlineColor:"white",
 			outlineWidth:2
-		};
+		},
 		// .. and this is the hover style. 
 		connectorHoverStyle = {
 			lineWidth:4,
 			strokeStyle:"#216477",
 			outlineWidth:2,
 			outlineColor:"white"
-		};
+		},
 		endpointHoverStyle = {
 			fillStyle:"#216477",
 			strokeStyle:"#216477"
 		};
 		// the definition of source endpoints (the small blue ones)
-		sourceEndpoint = {
+		this.sourceEndpoint = {
 			endpoint:"Dot",
 			paintStyle:{ 
 				strokeStyle:"#7AB02C",
@@ -314,7 +315,7 @@ function Flowchart(){
             ]
 		};		
 		// the definition of target endpoints (will appear when the user drags a connection) 
-		targetEndpoint = {
+		this.targetEndpoint = {
 			endpoint:"Dot",					
 			paintStyle:{ fillStyle:"#7AB02C",radius:11 },
 			hoverPaintStyle:endpointHoverStyle,
